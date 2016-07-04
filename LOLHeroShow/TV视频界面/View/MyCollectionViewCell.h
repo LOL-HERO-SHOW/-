f3f26bdbfc353373eb205f9a727cb2796c61e80b
bobo331386895 <331386895@qq.com>
2016-07-04 15:19:02 +0800

    改了key

--- LOLHeroShow/TV视频界面/View/MyCollectionViewCell.h
+++ LOLHeroShow/TV视频界面/View/MyCollectionViewCell.h
@@ -7,11 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "Video.h"
+
 @interface MyCollectionViewCell : UICollectionViewCell
 @property (weak, nonatomic) IBOutlet UIImageView *leftIV;
 @property (weak, nonatomic) IBOutlet UIImageView *myIV;
 @property (weak, nonatomic) IBOutlet UITextView *connectTV;
 
-@property(nonatomic,strong)Video *v;
+
 @end
